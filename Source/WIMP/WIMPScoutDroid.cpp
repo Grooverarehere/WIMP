2cf41c55c919768d885042abe6bc39d76258ff67
Gabriel Cabrera <gabycabper237@gmail.com>
2022-02-09 20:05:11 +0100

    First try droid scout

--- Source/WIMP/WIMPScoutDroid.cpp
+++ Source/WIMP/WIMPScoutDroid.cpp
@@ -9,6 +9,13 @@ AWIMPScoutDroid::AWIMPScoutDroid()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	RootComponent = SceneRoot;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	MovementTimelineComponent = CreateDefaultSubobject<UTimelineComponent>(TEXT("MovementTimelineComponent"));
 }
 
 // Called when the game starts or when spawned
@@ -16,12 +23,32 @@ void AWIMPScoutDroid::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	InitPosition = GetActorLocation();
+	EndPosition= EndPosition = FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z+Distance);
+	UpdateFunctionFloat.BindDynamic(this, &AWIMPScoutDroid::UpdateTimelineFunction);
+	if (MovementTimelineComponent)
+	{
+		MovementTimelineComponent->AddInterpFloat(MovementTimelineCurve, UpdateFunctionFloat);
+	}
+}
+
+void AWIMPScoutDroid::UpdateTimelineFunction(float Output)
+{
+	SetActorLocation(FMath::Lerp(InitPosition, EndPosition, Output));
 }
 
 // Called every frame
 void AWIMPScoutDroid::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (GetActorLocation().Equals(EndPosition))
+	{
+		MovementTimelineComponent->Reverse();
+	}
 
+	if (GetActorLocation().Equals(InitPosition))
+	{
+		MovementTimelineComponent->Play();
+	}
 }
 
--- Source/WIMP/WIMPScoutDroid.h
+++ Source/WIMP/WIMPScoutDroid.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
 #include "WIMPScoutDroid.generated.h"
 
 UCLASS()
@@ -15,10 +16,27 @@ public:
 	// Sets default values for this actor's properties
 	AWIMPScoutDroid();
 
+	UPROPERTY()
+		class USceneComponent* SceneRoot;
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* Mesh;
+	UPROPERTY(EditAnywhere)
+		float Distance;
+	UPROPERTY(EditAnywhere)
+		UCurveFloat* MovementTimelineCurve;
 protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UTimelineComponent* MovementTimelineComponent;
+	FOnTimelineFloat UpdateFunctionFloat;
+	FVector InitPosition;
+	FVector EndPosition;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
+	UFUNCTION()
+		void UpdateTimelineFunction(float Output);
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
